39a929a5ce5542c53181ed34cc0513e763dc84a5
Jan <github@sintho.com>
2015-09-22 16:41:16 +0200

    first tries at tilemaps

--- js/loading.js
+++ js/loading.js
@@ -7,8 +7,17 @@ loading.prototype = {
 	var loadingLabel = game.add.text(80,150, 'loading...',
 		{font: '30px Courier', fill: '#ffffff'});
 
+	/*
+	 * Menu images
+	 */
 	game.load.image('background','assets/background.png');
 	game.load.image('button', './assets/woodbutton.png');
+	
+	/*
+	 * Game images
+	 */
+	game.load.tilemap('level1', './assets/maze.json', null, Phaser.Tilemap.TILED_JSON);
+	game.load.image('level1Tiles', './assets/tiles.png');
 
 	},
 
--- js/play.js
+++ js/play.js
@@ -2,12 +2,20 @@ function play() {}
 
 play.prototype = {
 
-	preload : function() {
+	create : function() {
 
 		cursors = game.input.keyboard.createCursorKeys();
 
-		var background = game.add.image(0,0,'background');
+//		var background = game.add.image(0,0,'background');
+		
+		var map = game.add.tilemap('level1');
+        map.addTilesetImage('tiles', 'tiles');
+        
+        var layer = map.createLayer('Tile Layer 1');
 
+		
+//		map.setCollision(20, true, layer);
+		
 	}
 
 };
