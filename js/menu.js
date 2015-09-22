94f65d380b474ba2c0ff60943dd1e15bf5d58903
Jan <github@sintho.com>
2015-09-22 15:25:08 +0200

    small change in menu

--- js/menu.js
+++ js/menu.js
@@ -3,9 +3,11 @@ function menu() {}
 menu.prototype = {
 
 create:function() {
+	
+	game.add.sprite(0, 0, 'background');
 
-	var nameLabel = game.add.text(80,80,'Spiel',
-		{font: '50px Arial', fill: '#ffffff'});
+	var nameLabel = game.add.text(game.world.centerX - 150, 50,'Game Title',
+		{font: '75px Arial', fill: '#ffffff'});
 
 	var startLabel = game.add.text(80,game.world.height-100,'press the "W" key to start',
 		{font: '25px Arial', fill: '#ffffff'});
