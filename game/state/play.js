984251ff2685ad47fc2a93c156e0463b1b563c1d
Jan <github@sintho.com>
2015-09-24 12:04:38 +0200

    jumping through platforms from below

--- game/state/play.js
+++ game/state/play.js
@@ -47,16 +47,16 @@ play.prototype = {
 		scoreText.fixedToCamera = true;
 
 		/*
-		* add tools
-		*/
+		 * add tools
+		 */
 		this.nopliers = new Tools(this.game, 230, 15, 1);
 		this.game.add.existing(this.nopliers);
 		this.nopliers.fixedToCamera = true;
-	
+
 		this.nowrench = new Tools(this.game, 260, 15, 3);
 		this.game.add.existing(this.nowrench);
 		this.nowrench.fixedToCamera = true;
-	
+
 		this.noscrewdriver = new Tools(this.game, 290, 15, 5);
 		this.game.add.existing(this.noscrewdriver);
 		this.noscrewdriver.fixedToCamera = true;
@@ -64,7 +64,7 @@ play.prototype = {
 		this.collectpliers = new Tools(this.game, 200, 400, 0);
 		this.game.add.existing(this.collectpliers);
 		this.collectpliers.body.allowGravity = false;
-		
+
 		this.collectwrench = new Tools(this.game, 111, 400, 2);
 		this.game.add.existing(this.collectwrench);
 		this.collectwrench.body.allowGravity = false;
@@ -83,9 +83,12 @@ play.prototype = {
 		this.game.physics.arcade.collide(this.astronaut, this.alien,
 				this.collideWithAlien, null, this);
 
-		this.game.physics.arcade.overlap(this.astronaut, this.collectscrewdriver, this.collectTools, null, this);
-		this.game.physics.arcade.overlap(this.astronaut, this.collectwrench, this.collectTools, null, this);
-		this.game.physics.arcade.overlap(this.astronaut, this.collectpliers, this.collectTools, null, this);
+		this.game.physics.arcade.overlap(this.astronaut,
+				this.collectscrewdriver, this.collectTools, null, this);
+		this.game.physics.arcade.overlap(this.astronaut, this.collectwrench,
+				this.collectTools, null, this);
+		this.game.physics.arcade.overlap(this.astronaut, this.collectpliers,
+				this.collectTools, null, this);
 
 		/*
 		 * from http://phaser.io/examples/v2/arcade-physics/platformer-tight
@@ -136,17 +139,6 @@ play.prototype = {
 				+ 'ground');
 
 		/*
-		 * adds the character
-		 */
-		this.astronaut = new Astronaut(this.game, 100, 100);
-		this.game.add.existing(this.astronaut);
-		this.astronaut.animations.add('walk', [ 1, 2, 3, 4, 5 ], 20, true);
-		this.astronaut.animations.add('stop', [ 0 ], 20, true);
-		this.astronaut.anchor.setTo(0.5, 0.5);
-
-		this.game.camera.follow(this.astronaut);
-
-		/*
 		 * adds the tile map to the game !!tilemap json files need to be created
 		 * with the csv preset and not base64 compressed, or this won't work!!
 		 */
@@ -155,24 +147,42 @@ play.prototype = {
 		// the second parameter needs to be the same as the one used in
 		// loading.js
 		// TODO: image names need to be adjusted to the level number
-//		map.addTilesetImage('fmap-tiles', 'fmap-tiles');
-//		map.addTilesetImage('coin', 'coin');
-//		map.addTilesetImage('finish', 'finish')
+		// map.addTilesetImage('fmap-tiles', 'fmap-tiles');
+		// map.addTilesetImage('coin', 'coin');
+		// map.addTilesetImage('finish', 'finish')
 		map.addTilesetImage('level1_tilemap', 'level1_tilemap');
 
 		// TODO: amount of tiles needs to be the same for all levels - should be
 		// default
 		map.setCollisionBetween(1, 20);
 
-//		map.setTileIndexCallback(73, this.hitCoin, this);
-//
-//		map.setTileIndexCallback(79, this.hitFinish, this);
+		map.forEach(function(t) {
+			if (t) {
+				t.collideDown = false;
+			}
+		}, game, 0, 0, map.width, map.height, layer);
+
+		// map.setTileIndexCallback(73, this.hitCoin, this);
+		//
+		// map.setTileIndexCallback(79, this.hitFinish, this);
 
 		// the parameter can be found in the json file
 		layer = map.createLayer('Kachelebene 2');
 
 		// This resizes the game world to match the layer dimensions
 		layer.resizeWorld();
+
+		/*
+		 * adds the character
+		 */
+		this.astronaut = new Astronaut(this.game, 100, 450);
+		this.game.add.existing(this.astronaut);
+		this.astronaut.animations.add('walk', [ 1, 2, 3, 4, 5 ], 20, true);
+		this.astronaut.animations.add('stop', [ 0 ], 20, true);
+		this.astronaut.anchor.setTo(0.5, 0.5);
+
+		this.game.camera.follow(this.astronaut);
+
 	},
 
 	hitCoin : function(astronaut, tile) {
@@ -204,33 +214,32 @@ play.prototype = {
 
 	},
 
-	collectTools:function(astronaut, tools) {
+	collectTools : function(astronaut, tools) {
 
-	if(tools == this.collectpliers) {
-		console.log("pliers")
-		this.collectpliers.kill();
-		this.nopliers.kill();
-		this.pliers = new Tools(this.game, 230, 15, 0);
-		this.game.add.existing(this.pliers);
-		this.pliers.fixedToCamera = true;
-	}
-	if(tools == this.collectscrewdriver) {
-		console.log("screwdriver")
-		this.collectscrewdriver.kill();
-		this.noscrewdriver.kill();
-		this.screwdriver = new Tools(this.game, 290, 15, 4);
-		this.game.add.existing(this.screwdriver);
-		this.screwdriver.fixedToCamera = true;
-	}
-	if(tools == this.collectwrench) {
-		console.log("wrench")
-		this.collectwrench.kill();
-		this.nowrench.kill();
-		this.wrench = new Tools(this.game, 260, 15, 2);
-		this.game.add.existing(this.wrench);
-		this.wrench.fixedToCamera = true;
-	}
+		if (tools == this.collectpliers) {
+			console.log("pliers")
+			this.collectpliers.kill();
+			this.nopliers.kill();
+			this.pliers = new Tools(this.game, 230, 15, 0);
+			this.game.add.existing(this.pliers);
+			this.pliers.fixedToCamera = true;
+		}
+		if (tools == this.collectscrewdriver) {
+			console.log("screwdriver")
+			this.collectscrewdriver.kill();
+			this.noscrewdriver.kill();
+			this.screwdriver = new Tools(this.game, 290, 15, 4);
+			this.game.add.existing(this.screwdriver);
+			this.screwdriver.fixedToCamera = true;
+		}
+		if (tools == this.collectwrench) {
+			console.log("wrench")
+			this.collectwrench.kill();
+			this.nowrench.kill();
+			this.wrench = new Tools(this.game, 260, 15, 2);
+			this.game.add.existing(this.wrench);
+			this.wrench.fixedToCamera = true;
+		}
 	}
 
 };
-
