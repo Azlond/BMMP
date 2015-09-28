8d9ca487f52ff633bd417adaf8bf7244c1577aca
fyneira <teo.ferreira12@yahoo.com>
2015-09-28 14:12:10 +0200

    alien

--- game/comp/alien.js
+++ game/comp/alien.js
@@ -14,3 +14,14 @@ var Alien = function(game, x, y) {
 Alien.prototype = Object.create(Phaser.Sprite.prototype);  
 Alien.prototype.constructor = Alien;
 
+var level1alien1 = [ 600, 350 ];
+var level1alien2 = [ 1800, 350 ];
+var level2alien1 = [ 600, 350 ];
+var level2alien2 = [ 1600, 350 ];
+var level2alien3 = [ 1108, 350 ];
+var level3alien1 = [ 700, 350 ];
+var level3alien2 = [ 1600, 350 ];
+var level3alien3 = [ 1108, 350 ];
+var level4alien1 = [ 700, 350 ];
+var level4alien2 = [ 1600, 350 ];
+var level4alien3 = [ 1108, 350 ];
