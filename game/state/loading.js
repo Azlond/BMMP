bc7bbf05a936b8534f626de3b0ac74b538ca1365
SvenjaDittrich <svenja.dittrich@web.de>
2015-09-28 13:50:13 +0200

    alien kleiner im code

--- game/state/loading.js
+++ game/state/loading.js
@@ -87,7 +87,7 @@ loading.prototype = {
 		game.load.spritesheet('rocket', './assets/rocket_spritesheet.png', 154, 412, 2);
 		game.load.spritesheet('tools', './assets/Tools/alles.png', 59, 38, 6);
 		game.load.image('gameOver', './assets/gameOver.jpg');
-		game.load.spritesheet('alien', './assets/alien.png', 64, 64, 8);
+		game.load.spritesheet('alien', './assets/alien.png', 48, 48, 8);
 		game.load.spritesheet('tank', './assets/Status/oxygenStatusSprite.png', 43, 82);
 
 	},
